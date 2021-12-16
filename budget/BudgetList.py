8f255c596b81ed2dd793da54cce7b42cc310e753
Pär Sjöberg <paer.sjoberg@googlemail.com>
2021-12-16 19:13:43 +0100

    module 4 bugfix

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -42,8 +42,8 @@ def main():
 
     print('The count of all expenses: ' + str(len(myBudgetList)))
 
-    #for entry in myBudgetList:
-    #    print(entry)
+    for entry in myBudgetList:
+        print(entry)
 
     fig, ax = plt.subplots()
     labels = ['Expenses', 'Overages', 'Budget']
